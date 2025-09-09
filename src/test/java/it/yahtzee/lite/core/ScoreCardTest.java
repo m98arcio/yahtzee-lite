55d10a9b2354134dc272d5d2a5f7a9522e678edb
Marco Gallesi <marcogallesi@MacBook-Pro-di-Marco.local>
2025-09-09 15:20:06 +0200

    fix del test dello Score

--- src/test/java/it/yahtzee/lite/core/ScoreCardTest.java
+++ src/test/java/it/yahtzee/lite/core/ScoreCardTest.java
@@ -4,10 +4,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ScoreCardTest {
+
     @Test
-    void scoreSumIsCorrect() {
+    void scoreForCategoryWorks(){
         ScoreCard sc = new ScoreCard();
-        int s = sc.scoreSum(new int[]{1,2,3,4,5});
-        assertEquals(15, s);
+        int[] dice = {1,2,3,4,5};
+
+        assertEquals(1,  sc.scoreFor(ScoreCard.Category.ONES,   dice));
+        assertEquals(2,  sc.scoreFor(ScoreCard.Category.TWOS,   dice));
+        assertEquals(3,  sc.scoreFor(ScoreCard.Category.THREES, dice));
+        assertEquals(4,  sc.scoreFor(ScoreCard.Category.FOURS,  dice));
+        assertEquals(5,  sc.scoreFor(ScoreCard.Category.FIVES,  dice));
+        assertEquals(0,  sc.scoreFor(ScoreCard.Category.SIXES,  dice));
     }
 }
